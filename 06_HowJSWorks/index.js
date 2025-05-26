0dce91a666c16264da61fa399505833ff27e7557
Rahul Raj <rahul.raj9237@gmail.com>
2025-05-26 15:10:08 +0530

    inner working of JS

--- 06_HowJSWorks/index.js
+++ 06_HowJSWorks/index.js
@@ -5,14 +5,23 @@
 
 // console.log("Hii");
 
-const obj={
-    name:"Rahul",
-    greet:function(){
-        console.log(`Hello ${this.name}`);
+// const obj={
+//     name:"Rahul",
+//     greet:function(){
+//         console.log(`Hello ${this.name}`);
         
-    }
-}
-setTimeout((obj.greet),2000)
+//     }
+// }
+// setTimeout((obj.greet),2000)
 
-setTimeout(obj.greet.bind(obj),2000)
+// setTimeout(obj.greet.bind(obj),2000)
+
+console.log("hii");
+setTimeout(()=>console.log('after 2 sec'),2);
+Promise.resolve().then(()=>console.log('Resolved'));
+setTimeout(()=>console.log('after 2 s'),2);
+console.log("bye");
+
+
+
 
