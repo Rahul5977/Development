15c4b8c83e45da5a65bf24e0a19131f65c141b6b
Rahul Raj <rahul.raj9237@gmail.com>
2025-06-14 00:50:30 +0530

    Node internals

--- 13_backendInternals/index.js
+++ 13_backendInternals/index.js
@@ -1,9 +1,9 @@
-const fs=require('fs');
-const math = require('./math');
+const fs = require("fs");
+const math = require("./math");
 
-fs.writeFile('./test.txt','Hello World',()=>{})
+fs.writeFile("./test.txt", "Hello World", () => {});
 
-console.log(__filename,__dirname);
+console.log(__filename, __dirname);
 
-console.log(math.add(1,2))
+console.log(math.add(1,2));
 //jo udhar ka exports me hai vo yaha k require me chlega
--- 13_backendInternals/math.js
+++ 13_backendInternals/math.js
@@ -1,5 +1,14 @@
-function add(x, y) {
+const add = function (x, y) {
   return x + y;
-}
+};
+const sub = function (x, y) {
+  return x - y;
+};
+//default export
+module.exports = {
+  add,
+  sub,
+}; //now we can require with different name other than add sub
 
-exports.add = add;
+//per file only one default export ->name badal skte he
+//any nmber of named export ->name ni badal skte
