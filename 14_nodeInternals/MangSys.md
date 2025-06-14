31117dcf51ebcaae1169fbfa6f4217142d514ae8
Rahul Raj <rahul.raj9237@gmail.com>
2025-06-15 01:31:33 +0530

    DataBase disign

---
+++ 14_nodeInternals/MangSys.md
@@ -0,0 +1,55 @@
+# Book Management system
+
+### Tables 
+- User 
+    - ID
+    - Name
+    - Phone Number 
+    - Address 
+    - Role: 'Admin' or 'User'
+- Books
+    - ID
+    - Name
+    - ISBN_NUMBER
+    - Price
+- Author
+    - ID
+    - Name
+    - Instagram 
+    - TikTok
+    - List[Books]
+
+- BookRecords 
+    - ID
+    - Book ID
+    - User ID
+    - Issue Time 
+    - Is Returned 
+    - Return Time
+
+
+# Teachyst (Next Gen LMS)
+
+## Tables
+- User 
+- Courses 
+- Lessons
+    - ID
+    - Type: Live | Quiz
+- Enrollments 
+- Transactions  
+    - ID
+    - User ID
+    - Course ID
+    - Payment gateway
+    - Amount
+    - Date
+- Coupon
+    - Code
+    - Discount type
+    - Value
+    - Exp Time
+    - Start Time
+- Catergories 
+
+
--- 14_nodeInternals/index.js
+++ 14_nodeInternals/index.js
@@ -1,4 +1,30 @@
 const fs = require("fs");
+const crypto = require("crypto");
+process.env.UV_THREADPOOL_SIZE = 4;
 
 setTimeout(() => console.log("setTimeout"), 0);
-setImmediate(() => console.log(), 0);
+setImmediate(() => console.log("setImmediate in fs"));
+
+fs.readFile("sample.txt", "utf-8", (err, data) => {
+  setTimeout(() => console.log("setTimeout in fs"), 0);
+  setImmediate(() => console.log("setImmediate in fs"));
+  const start = Date.now();
+  crypto.pbkdf2("password", "salt1", 100000, 1024, "sha512", (err, data) => {
+    console.log(`[${Date.now() - start}]: password 1 Hashed`);
+  });
+  crypto.pbkdf2("password", "salt1", 100000, 1024, "sha512", (err, data) => {
+    console.log(`[${Date.now() - start}]: password 2 Hashed`);
+  });
+  crypto.pbkdf2("password", "salt1", 100000, 1024, "sha512", (err, data) => {
+    console.log(`[${Date.now() - start}]: password 3 Hashed`);
+  });
+  crypto.pbkdf2("password", "salt1", 100000, 1024, "sha512", (err, data) => {
+    console.log(`[${Date.now() - start}]: password 4 Hashed`);
+  });
+  crypto.pbkdf2("password", "salt1", 100000, 1024, "sha512", (err, data) => {
+    console.log(`[${Date.now() - start}]: password 5 Hashed`);
+  });
+  crypto.pbkdf2("password", "salt1", 100000, 1024, "sha512", (err, data) => {
+    console.log(`[${Date.now() - start}]: password 6 Hashed`);
+  });
+});
