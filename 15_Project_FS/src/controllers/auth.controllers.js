d465ff25816822ded23a53e1cd5dd89246fd71d6
Rahul Raj <rahul.raj9237@gmail.com>
2025-06-24 23:40:23 +0530

    all-controllers

--- 15_Project_FS/src/controllers/auth.controllers.js
+++ 15_Project_FS/src/controllers/auth.controllers.js
@@ -1,9 +1,53 @@
-import { asyncHandler } from "../utils/async-handler.js"
+import { asyncHandler } from "../utils/async-handler.js";
 
-const registerUser= asyncHandler(async (req,res)=>{
-    //get user data
-    const {email,username,password,role}=req.body
-    //validation
-    
-})
-export {registerUser}
+const registerUser = asyncHandler(async (req, res) => {
+  //get user data
+  const { email, username, password, role } = req.body;
+  //validation
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+  //get user data
+  const { email, username, password, role } = req.body;
+  //validation
+});
+const logOutUser = asyncHandler(async (req, res) => {
+  //get user data
+  const { email, username, password, role } = req.body;
+  //validation
+});
+
+const verifyEmail = asyncHandler(async (req, res) => {
+  //get user data
+  const { email, username, password, role } = req.body;
+  //validation
+});
+const resendEmailVerification = asyncHandler(async (req, res) => {
+  //get user data
+  const { email, username, password, role } = req.body;
+  //validation
+});
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  //get user data
+  const { email, username, password, role } = req.body;
+  //validation
+});
+
+const forgotPasswordRequest = asyncHandler(async (req, res) => {
+  //get user data
+  const { email, username, password, role } = req.body;
+  //validation
+});
+
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  //get user data
+  const { email, username, password, role } = req.body;
+  //validation
+});
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+  //get user data
+  const { email, username, password, role } = req.body;
+  //validation
+});
+export { registerUser };
