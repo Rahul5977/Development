01373cf132eadc84bfed86905a2a52f279d669d3
Rahul Raj <rahul.raj9237@gmail.com>
2025-06-25 18:54:01 +0530

    typescript done

---
+++ 17_TypeScriptIntro/typeScript.js
@@ -0,0 +1,20 @@
+// function add(x,y){
+//     return x+y;
+// }
+function addNum(a, b) {
+    return a + b;
+}
+console.log(addNum(1, 2));
+//javascript is loosly typed language
+var x = 10;
+x = "assjj";
+x = {
+    fname: "Raj",
+    lname: "me"
+};
+var p1 = {
+    fname: 'rahul'
+};
+function profile(p1) {
+    return p1.fname + p1.lname;
+}
---
+++ 17_TypeScriptIntro/typeScript.ts
@@ -0,0 +1,37 @@
+// // function add(x,y){
+// //     return x+y;
+// // }
+// function addNum(a:number,b:number){
+//     return a+b
+// }
+// console.log(addNum(1,2));
+
+
+// //javascript is loosly typed language
+
+// let x:number|string|object=10
+
+// x="assjj"
+// x={
+//     fname:"Raj",
+//     lname:"me"
+// }
+
+
+
+
+// interface user{
+//     fname:string,
+//     lname?:string
+// }
+
+// const p1={
+//     fname:'rahul'
+// }
+
+// function profile(p1:user){
+//     return p1.fname+p1.lname
+// }
+
+
+
